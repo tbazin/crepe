0df7c8fd80c2dc8a140f8504ca13812bb9e06b7a
Theis Bazin <theis.bazin@outlook.com>
2020-03-30 18:40:54 +0200

    :bug: Fix cents_mapping parameter unused

--- crepe/core.py
+++ crepe/core.py
@@ -133,11 +133,10 @@ def to_local_average_cents(salience, center=None):
     """
     find the weighted average cents near the argmax bin
     """
-
     if not hasattr(to_local_average_cents, 'cents_mapping'):
         # the bin number-to-cents mapping
-        to_local_average_cents.mapping = (
-                np.linspace(0, 7180, 360) + 1997.3794084376191)
+        to_local_average_cents.cents_mapping = (
+            np.linspace(0, 7180, 360) + 1997.3794084376191)
 
     if salience.ndim == 1:
         if center is None:
@@ -146,7 +145,7 @@ def to_local_average_cents(salience, center=None):
         end = min(len(salience), center + 5)
         salience = salience[start:end]
         product_sum = np.sum(
-            salience * to_local_average_cents.mapping[start:end])
+            salience * to_local_average_cents.cents_mapping[start:end])
         weight_sum = np.sum(salience)
         return product_sum / weight_sum
     if salience.ndim == 2:
