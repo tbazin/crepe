bdbfae2f079011a9dd8ae0ccc5c57bab1adba356
Theis Bazin <theis.bazin@outlook.com>
2020-03-30 18:44:02 +0200

    :white_check_mark: Add get_frames test

--- tests/test_sweep.py
+++ tests/test_sweep.py
@@ -34,6 +34,30 @@ def test_sweep_torch():
     verify_f0()
 
 
+# Test for frames slicing
+# normalizing disabled due to numerical discrepancies between numpy and PyTorch
+def test_get_frames_torch(normalize=False):
+    import torch
+    from crepe.torch_backend import DataHelper
+
+    try:
+        from scipy.io import wavfile
+        sr, audio = wavfile.read(file)
+    except ValueError:
+        import sys
+        print("CREPE: Could not read %s" % file, file=sys.stderr)
+        raise
+    frames_tf = crepe.core.get_frames(audio, sr, normalize=normalize)
+
+    audio_torch = torch.as_tensor(audio).unsqueeze(0)
+    data_helper = DataHelper(frame_duration_n=1024, hop_length_s=10e-3,
+                             center=True, normalize=normalize)
+    assert sr == data_helper.fs_hz
+    frames_torch = data_helper.get_frames(audio_torch)[0].numpy()
+
+    assert np.allclose(frames_tf, frames_torch)
+
+
 def test_activation_torch_tf():
     try:
         from scipy.io import wavfile
